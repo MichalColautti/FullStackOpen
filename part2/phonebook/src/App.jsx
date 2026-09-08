2749d9731184c71108ed1c1ea039a41e6dc08c1d
MichalC <m.colautti@o2.pl>
2026-09-08 22:56:54 +0200

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: '123-123-123' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
 
   const AddNewPerson = (event) => {
     event.preventDefault()
@@ -15,11 +16,13 @@ const App = () => {
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      phoneNumber: newPhoneNumber
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewPhoneNumber('')
   }
 
   return (
@@ -30,13 +33,16 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={newPhoneNumber} onChange={(e) => setNewPhoneNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit" onClick={AddNewPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>{person.name} {person.phoneNumber}</p>
         )}
       </ul>
     </div>
