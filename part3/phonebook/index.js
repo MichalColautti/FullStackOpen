998415d83343ec33bd01f6c4cf5050f657592041
MichalC <m.colautti@o2.pl>
2026-09-13 23:45:29 +0200

    3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -72,6 +72,12 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    if(persons.some((person) => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name already in phonebook'
+        })
+    }
+
     const newPerson = {
         id: generateId(),
         name: body.name,
