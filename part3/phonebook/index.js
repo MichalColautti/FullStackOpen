44b0f99005a816a7b4f8abe6ddf58acef8b5ed10
MichalC <m.colautti@o2.pl>
2026-09-13 23:42:23 +0200

    3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -55,6 +56,31 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter((person) => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    return String(Math.floor(Math.random() * 10000))
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
 })
 
 const PORT = 3001
